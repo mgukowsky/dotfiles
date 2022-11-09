20b375acd70adf52e8eaed85bdd6613f6f080516
Matt Gukowsky <mgukowsky@gmail.com>
2022-11-09 17:58:55 -0500

    Add nvidia_exrc.sh

---
+++ nvidia_exrc.sh
@@ -0,0 +1,5 @@
+# Shell configuration specific to Nvidia GPUs
+#
+# Prefix a vulkan executable invocation to use it with a specific driver
+# e.g. VK_NV vkcube
+alias VK_NV='VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json'
