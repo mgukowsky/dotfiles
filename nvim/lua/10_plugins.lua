1dc27d5c2950606dd6d8fa34d30cacc2722823be
Matt Gukowsky <mgukowsky@gmail.com>
2023-04-01 13:21:24 -0400

    Refactor nvim lua plugin management

--- nvim/lua/10_plugins.lua
+++ nvim/lua/10_plugins.lua
@@ -2,90 +2,88 @@
 
 -- Lua adapter for vim-plug
 -- Adapted from https://www.reddit.com/r/neovim/comments/xvbhs2/how_to_use_vim_plug_with_initlua/
+-- and some help from https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
 local Plug = vim.fn["plug#"]
 vim.call("plug#begin", vim.fn.stdpath("data") .. "/plugged")
 
 -- Recommended LSP configs
-Plug 'neovim/nvim-lspconfig'
+Plug('neovim/nvim-lspconfig')
 
 -- Vim-airline
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
+Plug('vim-airline/vim-airline')
+Plug('vim-airline/vim-airline-themes')
 
 -- Automatic completion for braces and other open/close characters
-Plug 'townk/vim-autoclose'
-Plug 'ryanoasis/vim-devicons'
+Plug('townk/vim-autoclose')
+Plug('ryanoasis/vim-devicons')
 
 -- Color schemes
-vim.cmd("Plug 'dracula/vim',{'as':'dracula'}") -- TODO: handle plugs with additional options in lua
-Plug 'morhetz/gruvbox'
-Plug 'junk-e/identity.vim'
-Plug 'Mizux/vim-colorschemes'
-Plug 'camgunz/amber'
-Plug 'kyoto-shift/film-noir'
-Plug 'pcostasgr/red_alert_vim_theme'
-Plug 'abnt713/vim-hashpunk'
-Plug 'TroyFletcher/vim-colors-synthwave'
-Plug 'wadackel/vim-dogrun'
-Plug 'joshdick/onedark.vim'
-Plug 'arcticicestudio/nord-vim'
-Plug 'Rigellute/rigel'
-Plug 'sainnhe/edge'
-Plug 'cocopon/iceberg.vim'
-Plug 'sainnhe/gruvbox-material'
-Plug 'sickill/vim-monokai'
-Plug 'Mofiqul/vscode.nvim'
+Plug('dracula/vim', { as = 'dracula' })
+Plug('morhetz/gruvbox')
+Plug('junk-e/identity.vim')
+Plug('Mizux/vim-colorschemes')
+Plug('camgunz/amber')
+Plug('kyoto-shift/film-noir')
+Plug('pcostasgr/red_alert_vim_theme')
+Plug('abnt713/vim-hashpunk')
+Plug('TroyFletcher/vim-colors-synthwave')
+Plug('wadackel/vim-dogrun')
+Plug('joshdick/onedark.vim')
+Plug('arcticicestudio/nord-vim')
+Plug('Rigellute/rigel')
+Plug('sainnhe/edge')
+Plug('cocopon/iceberg.vim')
+Plug('sainnhe/gruvbox-material')
+Plug('sickill/vim-monokai')
+Plug('Mofiqul/vscode.nvim')
 
-Plug 'scrooloose/nerdtree'
+Plug('scrooloose/nerdtree')
 
 -- Git support
-Plug 'lewis6991/gitsigns.nvim'
+Plug('lewis6991/gitsigns.nvim')
 
--- fzf integration
-vim.cmd("Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }") -- TODO: ditto
-Plug 'junegunn/fzf.vim'
+-- fzf integration; `[]` syntax is needed for `do`, since it's a reserved keyword
+Plug('junegunn/fzf', { ['do'] = function() vim.call('fzf#install') end })
+Plug('junegunn/fzf.vim')
 
 -- Adds :BufOnly to close all buffers except the current one
-Plug 'schickling/vim-bufonly'
+Plug('schickling/vim-bufonly')
 
 -- Makes <Leader>b display a listing of all buffers
-Plug 'jeetsukumaran/vim-buffergator'
+Plug('jeetsukumaran/vim-buffergator')
 
 -- Comment/uncomment a line with 'gcc', or a selection with 'gc'
-Plug 'tpope/vim-commentary'
+Plug('tpope/vim-commentary')
 
 -- Minimize visual noise with :Goyo and :Limelight
-Plug 'junegunn/goyo.vim'
-Plug 'junegunn/limelight.vim'
+Plug('junegunn/goyo.vim')
+Plug('junegunn/limelight.vim')
 
-Plug 'tpope/vim-markdown'
-Plug 'neui/cmakecache-syntax.vim'
-Plug 'tpope/vim-surround'
-Plug 'cespare/vim-toml'
-Plug 'dag/vim-fish'
+Plug('tpope/vim-markdown')
+Plug('neui/cmakecache-syntax.vim')
+Plug('tpope/vim-surround')
+Plug('cespare/vim-toml')
+Plug('dag/vim-fish')
 
 -- Have vim insert a matching 'end', 'endif', 'endfunction', etc.
-Plug 'tpope/vim-endwise'
+Plug('tpope/vim-endwise')
 
 -- Perform autocompletion with just TAB
-Plug 'ervandew/supertab'
-
--- Lightweight personal vim wiki
-Plug 'vimwiki/vimwiki'
+Plug('ervandew/supertab')
 
 -- Animate cursor jumps
-Plug 'danilamihailov/beacon.nvim'
+Plug('danilamihailov/beacon.nvim')
 
 -- Show vertical lines for each indent level
-Plug 'Yggdroot/indentLine'
+Plug('Yggdroot/indentLine')
 
 -- Underline the word under the cursor
-Plug 'yamatsum/nvim-cursorline'
+Plug('yamatsum/nvim-cursorline')
 
 -- Show marks in the sign column
-Plug 'chentoast/marks.nvim'
+Plug('chentoast/marks.nvim')
 
 -- nvim-treesitter for better syntax highlighting
-vim.cmd("Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}") -- TODO: ditto
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = function() vim.cmd('TSUpdate') end })
 
 vim.call("plug#end")
