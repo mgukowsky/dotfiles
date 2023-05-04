8026a78e26ae0d2395c2d43510423e3baf45a0d2
Matt Gukowsky <mgukowsky@gmail.com>
2023-05-04 11:20:24 -0400

    Remove <CR> mapping from nvim-cmp config, since that seems to break <CR> functionality in some menus

--- nvim/lua/20_opts.lua
+++ nvim/lua/20_opts.lua
@@ -198,18 +198,9 @@ cmp.setup({
         fallback()
       end
     end, { "i", "s" }),
-    -- Make <CR> complete the entry if one is selected, otherwise make a newline like normal; also from the nvim-cmp wiki
-    ["<CR>"] = cmp.mapping({
-      i = function(fallback)
-        if cmp.visible() and cmp.get_active_entry() then
-          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-        else
-          fallback()
-        end
-      end,
-      s = cmp.mapping.confirm({ select = true }),
-      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-    }),
+    -- The wiki also includes a snippet to make <CR> complete the entry if one is selected and
+    -- otherwise make a newline like normal. I've chosen to omit this, however, since this seems to
+    -- break <CR> functionality in some, such as the LSPMenu I create in 50_lua.lsp
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp',               keyword_length = 1 },
