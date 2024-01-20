52199c49beacb23dad0b9739363a184a521feb43
Matt Gukowsky <mgukowsky@gmail.com>
2024-01-20 17:56:27 -0500

    Add cmake LSP

--- nvim/lua/50_lsp.lua
+++ nvim/lua/50_lsp.lua
@@ -195,7 +195,7 @@ lspconfigs.ltex.setup({
 })
 
 -- Setup LSPs that don't require any additional configs
-for _, lsp_name in pairs({ "clangd", "jedi_language_server", "ruby_ls", "tsserver" }) do
+for _, lsp_name in pairs({ "clangd", "cmake", "jedi_language_server", "ruby_ls", "tsserver" }) do
   lspconfigs[lsp_name].setup({
     on_attach = on_attach,
     capabilities = nvimCmpCapabilities,
