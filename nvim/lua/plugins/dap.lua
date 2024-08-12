6d302e0a865d1780a897b948ddecaf51c09e092d
Matt Gukowsky <mgukowsky@gmail.com>
2024-08-11 22:42:46 -0400

    Add toggleterm

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -92,74 +92,92 @@ return {
         },
         keys = {
           { "<leader>oc", function() require("overseer").close() end, desc = "Close" },
-          { "<leader>oo", function() require("overseer").open() end, desc = "Open" },
-          { "<leader>or", function()
-            local overseer = require("overseer")
-            overseer.open()
-            overseer.run_template()
-          end, desc = "Run" },
-          -- { "<leader>ot", function() require("overseer").toggle() end, desc = "Toggle" },
-          { "<leader>ot", function() require("overseer").toggleterm({direction = "float"}) end, desc = "Toggle" },
+          { "<leader>oo", function() require("overseer").open() end,  desc = "Open" },
+          {
+            "<leader>or",
+            function()
+              local overseer = require("overseer")
+              overseer.open()
+              overseer.run_template()
+            end,
+            desc = "Run"
+          },
+          { "<leader>ot", function() require("overseer").toggle() end, desc = "Toggle" },
         }
       },
     },
     keys = {
-      {"<F5>", function() require("dap").continue() end, desc = "Continue (DAP)"},
-      {"<F7>",
+      { "<F5>",        function() require("dap").continue() end,          desc = "Continue (DAP)" },
+      {
+        "<F7>",
         function()
           local overseer = require("overseer")
           overseer.open()
           overseer.run_template()
-        end, desc = "Run overseer template"},
-      {"<F9>", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint"},
-      {"<F10>", function() require("dap").step_over() end, desc = "Step over"},
-      {"<F11>", function() require("dap").step_into() end, desc = "Step into"},
+        end,
+        desc = "Run overseer template"
+      },
+      { "<F9>",        function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint" },
+      { "<F10>",       function() require("dap").step_over() end,         desc = "Step over" },
+      { "<F11>",       function() require("dap").step_into() end,         desc = "Step into" },
       -- This is the keysym for Shift+F11 (i.e. F12 + 11)
-      {"<F23>", function() require("dap").step_out() end, desc = "Step out"},
+      { "<F23>",       function() require("dap").step_out() end,          desc = "Step out" },
 
-      {"<leader>dbb", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint"},
+      { "<leader>dbb", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint" },
 
       -- Per https://www.reddit.com/r/neovim/comments/15dtb8l/comment/ju4u4j7
       -- Seems like variables can be referenced directly and don't need the {} wrapping
       --  e.g. `i > 5` is OK;
-      {"<leader>dbc", function()
-        vim.ui.input({ prompt = "Breakpoint condition" }, function(input)
-          require("dap").set_breakpoint(input)
-        end)
-      end, desc = "Conditional breakpoint"},
-      {"<leader>dbh", function()
-        vim.ui.input({ prompt = "Hit condition (e.g. how many hits before stopping)" }, function(input)
-          require("dap").set_breakpoint(nil, input)
-        end)
-      end, desc = "Hit conditional breakpoint"},
+      {
+        "<leader>dbc",
+        function()
+          vim.ui.input({ prompt = "Breakpoint condition" }, function(input)
+            require("dap").set_breakpoint(input)
+          end)
+        end,
+        desc = "Conditional breakpoint"
+      },
+      {
+        "<leader>dbh",
+        function()
+          vim.ui.input({ prompt = "Hit condition (e.g. how many hits before stopping)" }, function(input)
+            require("dap").set_breakpoint(nil, input)
+          end)
+        end,
+        desc = "Hit conditional breakpoint"
+      },
 
       -- Requires the {} dereferencing syntax
       --  e.g. `num is {num}` is necessary
-      {"<leader>dbl", function()
-        vim.ui.input({ prompt = "Trace log message" }, function(input)
-          require("dap").set_breakpoint(nil, nil, input)
-        end)
-      end, desc = "Set breakpoint with log message"},
+      {
+        "<leader>dbl",
+        function()
+          vim.ui.input({ prompt = "Trace log message" }, function(input)
+            require("dap").set_breakpoint(nil, nil, input)
+          end)
+        end,
+        desc = "Set breakpoint with log message"
+      },
 
-      {"<leader>dc", function() require("dap").continue() end, desc = "Start/Continue execution"},
+      { "<leader>dc",  function() require("dap").continue() end,                              desc = "Start/Continue execution" },
 
-      {"<leader>dfc", function() require("dap").focus_frame() end, desc = "Go to current (active) frame"},
-      {"<leader>dfd", function() require("dap").down() end, desc = "Go down one frame"},
-      {"<leader>dfr", function() require("dap").restart_frame() end, desc = "Restart execution of the current frame"},
-      {"<leader>dfu", function() require("dap").up() end, desc = "Go up one frame"},
+      { "<leader>dfc", function() require("dap").focus_frame() end,                           desc = "Go to current (active) frame" },
+      { "<leader>dfd", function() require("dap").down() end,                                  desc = "Go down one frame" },
+      { "<leader>dfr", function() require("dap").restart_frame() end,                         desc = "Restart execution of the current frame" },
+      { "<leader>dfu", function() require("dap").up() end,                                    desc = "Go up one frame" },
 
-      {"<leader>dn", function() require("dap").step_over() end, desc = "Step Over"}, -- "next"
-      {"<leader>do", function() require("dap").step_out() end, desc = "Step Out"}, -- "finish"
-      {"<leader>dp", function() require("dap").pause() end, desc = "Pause execution"},
-      {"<leader>ds", function() require("dap").step_into() end, desc = "Step Into"},
-      {"<leader>du", function() require("dap").run_to_cursor() end, desc = "Run until current line"}, -- this will temporarily disable breakpoints
-      {"<leader>dx", function() require("dap").terminate() end, desc = "Terminate running program"},
+      { "<leader>dn",  function() require("dap").step_over() end,                             desc = "Step Over" }, -- "next"
+      { "<leader>do",  function() require("dap").step_out() end,                              desc = "Step Out" },  -- "finish"
+      { "<leader>dp",  function() require("dap").pause() end,                                 desc = "Pause execution" },
+      { "<leader>ds",  function() require("dap").step_into() end,                             desc = "Step Into" },
+      { "<leader>du",  function() require("dap").run_to_cursor() end,                         desc = "Run until current line" }, -- this will temporarily disable breakpoints
+      { "<leader>dx",  function() require("dap").terminate() end,                             desc = "Terminate running program" },
 
-      {"<leader>dqq", function() require("telescope").extensions.dap.commands() end, desc = "Command search"},
-      {"<leader>dqb", function() require("telescope").extensions.dap.list_breakpoints() end, desc = "List breakpoints"},
-      {"<leader>dqc", function() require("telescope").extensions.dap.configurations() end, desc = "Select configuration"},
-      {"<leader>dqf", function() require("telescope").extensions.dap.frames() end, desc = "List frames"},
-      {"<leader>dqv", function() require("telescope").extensions.dap.variables() end, desc = "List variables"},
+      { "<leader>dqq", function() require("telescope").extensions.dap.commands() end,         desc = "Command search" },
+      { "<leader>dqb", function() require("telescope").extensions.dap.list_breakpoints() end, desc = "List breakpoints" },
+      { "<leader>dqc", function() require("telescope").extensions.dap.configurations() end,   desc = "Select configuration" },
+      { "<leader>dqf", function() require("telescope").extensions.dap.frames() end,           desc = "List frames" },
+      { "<leader>dqv", function() require("telescope").extensions.dap.variables() end,        desc = "List variables" },
     },
     config = function()
       install_dap_signs()
@@ -169,8 +187,8 @@ return {
     "rcarriga/nvim-dap-ui",
     dependencies = { "nvim-neotest/nvim-nio" },
     keys = {
-      {"<leader>dt", function() require("dapui").toggle() end, desc = "Toggle DAP UI"},
-      {"<leader>d]", function() require("dapui").eval(nil, { enter = true }) end, desc = "Evaluate variable under cursor/highlight"},
+      { "<leader>dt", function() require("dapui").toggle() end,                    desc = "Toggle DAP UI" },
+      { "<leader>d]", function() require("dapui").eval(nil, { enter = true }) end, desc = "Evaluate variable under cursor/highlight" },
     },
     config = function()
       setup_dap()
@@ -198,8 +216,8 @@ return {
     "mfussenegger/nvim-dap-python",
     ft = "python",
     keys = {
-      {"<leader>dqd", function() require("dap-python").test_class() end, desc = "Test class under cursor"},
-      {"<leader>dqD", function() require("dap-python").test_method() end, desc = "Test method under cursor"},
+      { "<leader>dqd", function() require("dap-python").test_class() end,  desc = "Test class under cursor" },
+      { "<leader>dqD", function() require("dap-python").test_method() end, desc = "Test method under cursor" },
     },
     config = function()
       local pypath = require("local.util").stdout_exec("which python3")
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -5,9 +5,12 @@ return {
     float_opts = {
       border = "double",
     },
+    on_open = function(term)
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    end,
   },
   keys = {
-		-- Similar to VSCode Ctrl+`
-    {"<leader>`", function() require("toggleterm").toggle() end, desc = "Toggle terminal"}
+    -- Similar to VSCode Ctrl+`
+    { "<leader>`", function() require("toggleterm").toggle() end, desc = "Toggle terminal" }
   },
 }
