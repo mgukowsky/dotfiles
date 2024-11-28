7e06c48b2b2d8d2f020747037c2ddfa9b5051620
Matt Gukowsky <mgukowsky@gmail.com>
2024-11-27 22:00:39 -0500

    Put nvim logs into /tmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,19 @@
+-- Before we do anything else, let's put logs in /tmp.
+-- I don't need logs to be persistent, and I don't want to have
+-- to monitor/manage an ever-expanding log file (esp. true
+-- of LSP logs).
+local NEW_TMP_PATH = "/tmp/nvim/logs"
+vim.fn.mkdir(NEW_TMP_PATH, "p")
+
+-- Per https://www.reddit.com/r/neovim/comments/s2t0ph/comment/hsgqrv6
+local old_stdpath = vim.fn.stdpath
+vim.fn.stdpath = function(value)
+  if value == "log" then
+    return NEW_TMP_PATH
+  end
+  return old_stdpath(value)
+end
+
 local lazy = require("config.lazy")
 lazy.bootstrap_lazy()
 
