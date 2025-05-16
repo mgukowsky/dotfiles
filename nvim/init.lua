e73778462be3dfccf79424166b93479fd9d8a122
Matt Gukowsky <mgukowsky@gmail.com>
2025-05-16 13:04:24 -0400

    Use space for <leader>

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,7 +23,7 @@ lazy.bootstrap_lazy()
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = "\\"
+vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 require("config.opts")
 require("config.mappings")
